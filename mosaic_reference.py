120c6cd6b8505f7064e5240eed0eb74755a3eee1
jihyeok1022 <144329012+jihyeok1022@users.noreply.github.com>
2023-12-14 15:55:25 +0900

    Update mosaic_reference.py
    위치에 따른 얼굴 크기 차이에 따라 뒤에 있는 사람만 모자이크 되게 함

--- mosaic_reference.py
+++ mosaic_reference.py
@@ -1,31 +1,38 @@
 import cv2
-import numpy
 
+# 웹캠 열기
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-cap.set(3,640) # 너비
-cap.set(4,480) # 높이
+# 얼굴 감지용 분류기 로드
+face_cascade = cv2.CascadeClassifier("C:\\opensource\\haarcascade_frontalface_default.xml")
 
-while(cap.isOpened()):
-    ret, frame = cap.read() 
-    frame = cv2.flip(frame, 1) # 좌우 대칭
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    # 그레이스케일로 변환
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray,1.05,5) 
+    # scaleFactor 및 minSize 조정
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minSize=(30, 30))
+
     print("Number of faces detected: " + str(len(faces)))
 
     if len(faces):
-        for (x,y,w,h) in faces:
-            face_img = frame[y:y+h, x:x+w] # 탐지된 얼굴 이미지 crop
-            face_img = cv2.resize(face_img, dsize=(0, 0), fx=0.04, fy=0.04) # 축소
-            face_img = cv2.resize(face_img, (w, h), interpolation=cv2.INTER_AREA) # 확대
-            frame[y:y+h, x:x+w] = face_img # 탐지된 얼굴 영역 모자이크 처리
+        # 제일 큰 얼굴은 모자이크를 적용하지 않음
+        max_face = max(faces, key=lambda face: face[2] * face[3])
+        for (x, y, w, h) in faces:
+            if (x, y, w, h) != tuple(max_face):
+                face_img = frame[y:y + h, x:x + w]  # 탐지된 얼굴 이미지 crop
+                face_img = cv2.resize(face_img, dsize=(0, 0), fx=0.04, fy=0.04)  # 축소
+                face_img = cv2.resize(face_img, (w, h), interpolation=cv2.INTER_AREA)  # 확대
+                frame[y:y + h, x:x + w] = face_img  # 탐지된 얼굴 영역 모자이크 처리
 
+    # 결과 표시
     cv2.imshow('result', frame)
-        
-    if(cv2.waitKey(25) & 0xFF == ord("q")):
+
+    if cv2.waitKey(25) & 0xFF == ord("q"):
         break
 
+# 웹캠 해제
 cap.release()
 cv2.destroyAllWindows()
