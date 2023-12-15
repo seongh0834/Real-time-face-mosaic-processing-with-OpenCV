8abe966062b76e8110ca87402fb7c6170570910a
Seonghak Song <seongh0834@gachon.ac.kr>
2023-12-15 22:08:30 +0900

    integrated code + small changes

--- mosaic_reference.py
+++ mosaic_reference.py
@@ -1,13 +1,13 @@
 import cv2
-import numpy
 
 # 웹캠 열기
 cap = cv2.VideoCapture(0)
+
 # 얼굴 감지용 분류기 로드
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-cap.set(3,640) # 너비
-cap.set(4,480) # 높이
+cap.set(3,1280) # 너비
+cap.set(4,720) # 높이
 
 while(cap.isOpened()):
     ret, frame = cap.read()
@@ -20,8 +20,7 @@ while(cap.isOpened()):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # scaleFactor 및 minSize 조정
-    #faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minSize=(30, 30))
-    faces = face_cascade.detectMultiScale(gray,1.05,5) # 얼굴 감지
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minSize=(30, 30))
 
     centers = [] # 얼굴 감지 후 얼굴 중심점을 계산
     for (x, y, w, h) in faces:
@@ -30,14 +29,14 @@ while(cap.isOpened()):
         centers.append((center_x, center_y))
 
     print("Number of faces detected: " + str(len(faces)))
-    print(centers)
+    print("faces center coordinates: " + str(centers))
 
     if len(faces):
         # 제일 큰 얼굴은 모자이크를 적용하지 않음
         max_face = max(faces, key=lambda face: face[2] * face[3])
 
         for (x, y, w, h) in faces:
-            if (((x, y, w, h) != tuple(max_face)) and (w<100 or h<100)):
+            if ((x, y, w, h) != tuple(max_face)):
                 face_img = frame[y:y + h, x:x + w]  # 탐지된 얼굴 이미지 crop
                 face_img = cv2.resize(face_img, dsize=(0, 0), fx=0.04, fy=0.04)  # 축소
                 face_img = cv2.resize(face_img, (w, h), interpolation=cv2.INTER_AREA)  # 확대
